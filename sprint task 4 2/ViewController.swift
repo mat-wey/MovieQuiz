e02cc628874b643d7cb7fe1f305d51be5b41d9c4
Practicum <practicum@ya.ru>
2025-06-25 18:40:21 +0300

    Initial commit

---
+++ sprint task 4 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sprint task 4 2
+//
+//  Created by Матвей Симонов on 25.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
