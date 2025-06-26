1fc4d4eb41f0440736b8e7c9ee0bdd5741afb4a4
Practicum <practicum@ya.ru>
2025-06-26 12:12:30 +0300

    commit

--- sprint task 4 2/ViewController.swift
+++ sprint task 4 2/ViewController.swift
@@ -8,23 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    @IBOutlet weak var вопрос: UILabel!
     @IBOutlet weak var counts: UILabel!
     @IBOutlet weak var buttonДа: UIButton!
+    @IBOutlet weak var QuestionTitleLabel: UILabel!
+    
     @IBOutlet weak var buttonНет: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         counts.text = "0/10"
     }
-    private var count = 0
-    @IBAction func buttonPlus1Нет(_ sender: Any) {
-        count += 1
-        counts.text = "\(count)/\(10)"
-    }
-    
-    @IBAction func buttonPlus1Да(_ sender: Any) {
-        count += 1
-        counts.text = "\(count)/\(10)"
-    }
+    private let questions : [QuizQuestion] = [QuizQuestion(
+        image: "The Godfather",
+        text: "Рейтинг этого фильма больше чем 6?",
+        correctAnswer: true),QuizQuestion(
+            image: "The dark knight",
+            text: "Рейтинг этого фильма больше чем 6?",
+            correctAnswer: true),
+                    QuizQuestion(image: "Bill kill",text:"Рейтинг этого фильма больше чем 6?",correctAnswer: true),
+                    QuizQuestion(
+                        image: "The Avengers",
+                        text: "Рейтинг этого фильма больше чем 6?",
+                        correctAnswer: true),
+                    QuizQuestion(
+                        image: "Deadpool",
+                        text: "Рейтинг этого фильма больше чем 6?",
+                        correctAnswer: true),
+                    QuizQuestion(
+                        image: "The Green Knight",
+                        text: "Рейтинг этого фильма больше чем 6?",
+                        correctAnswer: true),
+                    QuizQuestion(
+                        image: "Old",
+                        text: "Рейтинг этого фильма больше чем 6?",
+                        correctAnswer: false),
+                    QuizQuestion(
+                        image: "The Ice Age Adventures of Buck Wild",
+                        text: "Рейтинг этого фильма больше чем 6?",
+                        correctAnswer: false),
+                    QuizQuestion(
+                        image: "Tesla",
+                        text: "Рейтинг этого фильма больше чем 6?",
+                        correctAnswer: false),
+                    QuizQuestion(
+                        image: "Vivarium",
+                        text: "Рейтинг этого фильма больше чем 6?",
+                        correctAnswer: false)
+                ]
+        
 }
 
