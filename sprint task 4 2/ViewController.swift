7d37b4b6d816d167250afadfdc1fdc7907891327
Practicum <practicum@ya.ru>
2025-06-26 17:51:45 +0300

    set the correct constraints

--- sprint task 4 2/ViewController.swift
+++ sprint task 4 2/ViewController.swift
@@ -12,13 +12,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var counts: UILabel!
     @IBOutlet weak var buttonДа: UIButton!
     @IBOutlet weak var QuestionTitleLabel: UILabel!
-    
     @IBOutlet weak var buttonНет: UIButton!
+    private var correctAnswers = 0
+    private var QuestionIndex = 0
+    private let font = UIFont(name: "YSDisplay-Medium", size: 20)
     override func viewDidLoad() {
         super.viewDidLoad()
         counts.text = "0/10"
+        QuestionTitleLabel.font = font
+        вопрос.font = font
     }
-    private let questions : [QuizQuestion] = [QuizQuestion(
+    private let questions: [QuizQuestion] = [QuizQuestion(
         image: "The Godfather",
         text: "Рейтинг этого фильма больше чем 6?",
         correctAnswer: true),QuizQuestion(
@@ -55,6 +59,14 @@ class ViewController: UIViewController {
                         text: "Рейтинг этого фильма больше чем 6?",
                         correctAnswer: false)
                 ]
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        let a = QuizStepViewModel(
+            image: UIImage(
+                named: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(QuestionIndex + 1)\(questions.count)")
+        return a
+    }
         
 }
 
